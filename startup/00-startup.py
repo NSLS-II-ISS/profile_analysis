0dd84cc64ad5a98781f28818cd3b9bdfdd03456d
elistavitski <elistavitski@gmail.com>
2018-04-04 11:50:41 -0400

    Initial commit

---
+++ startup/00-startup.py
@@ -0,0 +1,17 @@
+import databroker
+from PyQt5.QtWidgets import QApplication
+import sys
+from isstools import Xview
+
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+db = databroker.Broker.named('iss')
+
+iss_mono_pulses_per_deg= 360000
+app = QApplication(sys.argv)
+xview_gui = Xview.XviewGui(iss_mono_pulses_per_deg, db=db)
+
+def xview():
+    xview_gui.show()
